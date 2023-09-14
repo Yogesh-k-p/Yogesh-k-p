4e9608556bef95cf3279945a54b329fe27148bcc
Yogesh-k-p <142667002+Yogesh-k-p@users.noreply.github.com>
2023-09-14 15:52:49 +0530

    Update Slicing.py

--- Slicing.py
+++ Slicing.py
@@ -1,15 +1,16 @@
 # Email-Slicer-with-Python # 
 # Email input #
+# different methods to achieve the same result #
+
+## 1) Using String Indexing
 
 email = input("Enter Your Email: ").strip()
-
 username = email[:email.index('@')]
 domain = email[email.index('@') + 1:]
-
 print(f"Your username is {username} & domain is {domain}")
 
+## 2) Using the partition Method
 
-# Email slicer #
 
 def email_slicer(email):
     username, _, domain = email.strip().partition("@")
@@ -18,10 +19,13 @@ user_input = input("Enter Your Email: ")
 print(email_slicer(user_input))
 
 
+## 3) Using the partition Method (Alternative Syntax):
 
 email_address = input(“enter your email address”).strip()
 new = email_address.partition(“@”)
 print(f”user name {new[0] } and {new[-1]}”)
+
+## 4) Using String Slicing and format Method:
 
 
 email = input("Enter Your Email: ").strip()
@@ -30,23 +34,14 @@ domain_name = email[email.index("@")+1:]
 format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
 print(format_)
 
+## 5) Using String Slicing and Concatenation:
 
-# Get user email address
+
 email = input("What is your email address?: ").strip()
-
-# Slice out the user name
 user_name = email[:email.index("@")]
-
-# Slice the domain name
-domain_name = email[email.index("@")+1:]
-
-# Format message
-output = "Your username is '{}' and your domain name is '{}'".format(user_name,domain_name)
-
-# Display output message
+domain_name = email[email.index("@") + 1:]
+output = "Your username is '{}' and your domain name is '{}'".format(user_name, domain_name)
 print(output)
-
-
 
 
 
